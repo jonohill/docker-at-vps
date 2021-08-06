152d6ce021552ce2fa7904c5492b3589591bcf4a
Jono Hill <jono@hillnz.com>
2021-08-06 19:59:56 +1200

    Fix entrypoint

--- config/rclone_serve_backup/entrypoint.sh
+++ config/rclone_serve_backup/entrypoint.sh
@@ -6,8 +6,8 @@ if [ ! -f "$RCLONE_CONF" ]; then
     echo "$RCLONE_CONFIG_SEED" | base64 -d >"$RCLONE_CONF"
 fi
 
-exec nice rclone \
-    -n 10 rclone \
+exec nice -n 10 \
+    rclone \
     serve sftp \
     --addr :2022 \
     --vfs-cache-mode writes \
