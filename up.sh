37884d0b05560298871fe81973f77c936e754052
Jono Hill <jono@hillnz.com>
2021-08-29 11:14:53 +1200

    Bring up tailscale early

--- up.sh
+++ up.sh
@@ -3,4 +3,5 @@
 set -e
 
 git pull
+SOPS_AGE_KEY_FILE=key.txt sops exec-env secrets.enc.env 'docker-compose up -d tailscale' || true
 SOPS_AGE_KEY_FILE=key.txt sops exec-env secrets.enc.env 'docker-compose up --remove-orphans -d'
