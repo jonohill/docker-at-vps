96513802eeedf3393d7af017ca8709ca7e87e1da
Jono Hill <jono@hillnz.com>
2021-11-07 18:06:04 +1300

    Add convenience scripts

--- up.sh
+++ up.sh
@@ -3,5 +3,5 @@
 set -e
 
 git pull
-SOPS_AGE_KEY_FILE=key.txt sops exec-env secrets.enc.env 'docker-compose up -d tailscale' || true
-SOPS_AGE_KEY_FILE=key.txt sops exec-env secrets.enc.env 'docker-compose up --remove-orphans -d'
+./compose up -d tailscale || true
+./compose up --remove-orphans -d
